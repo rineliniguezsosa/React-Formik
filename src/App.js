425001044fe25d137b97d91f41fbd9b0d704b3fe
rinel <rineliniguezsosa@gmail.com>
2022-02-03 21:45:22 -0600

    prop apellido html

--- src/App.js
+++ src/App.js
@@ -48,9 +48,8 @@ function App() {
                       </label>
                       <input
                         type="text"
-                        name="last-name"
-                        id="last-name"
-                        autoComplete="family-name"
+                        name="apellido"
+                        id="apellido"
                         className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                       />
                     </div>

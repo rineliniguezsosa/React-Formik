009621824f224f27b7394b1ef96f0179d25a07a4
rinel <rineliniguezsosa@gmail.com>
2022-02-03 21:51:24 -0600

    prop cuidad html name

--- src/App.js
+++ src/App.js
@@ -71,9 +71,8 @@ function App() {
                         Cuidad
                       </label>
                       <select
-                        id="country"
-                        name="country"
-                        autoComplete="country-name"
+                        id="cuidad"
+                        name="cuidad"
                         className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                       >
                         <option>United States</option>

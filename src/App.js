62dea7d3f5899009a2ded95e8ea882d5e256f93f
rinel <rineliniguezsosa@gmail.com>
2022-02-03 22:48:02 -0600

    componente ErrorMessage

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ function App() {
                         id="nombre"
                         className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                       />
+                      <ErrorMessage name="nombre"></ErrorMessage>
                     </div>
 
                     <div className="col-span-6 sm:col-span-3">

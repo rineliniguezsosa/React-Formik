03ada1f739e36a7e3518e8bddadbb1fbadff58eb
rinel <rineliniguezsosa@gmail.com>
2022-02-04 19:24:16 -0600

    evitando el submit

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
             <Formik 
             initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}
             validateOnChange={false} validateOnBlur={false} >
-            {({values})=>(
+            {({isSubmitting,values})=>(
             <Form >
               <div className="shadow overflow-hidden sm:rounded-md">
                 <div className="px-4 py-5 bg-white sm:p-6">
@@ -153,6 +153,7 @@ function App() {
                 </div>
                 <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                   <button
+                    disabled={isSubmitting}
                     type="submit"
                     className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
